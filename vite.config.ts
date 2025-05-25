4813150e5639f7fd3673deb811fbe755b6032cd6
Yunjeong <yunjeongiya@gmail.com>
2025-05-25 15:32:53 +0900

    Configure Github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/checkus-teacher-web/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
